16a7f35648e8ac5c2d0cb1360e6147d45af4f5f8
Carlos Dario Almonte <c.darioalmonte@gmail.com>
2023-04-02 21:06:51 -0400

    feat: enable chatting through the terminal
    #1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 import { ChatGPTAPI } from "chatgpt";
+import { createInterface } from "readline";
 
 const makeRequest = async () => {
-  const api = new ChatGPTAPI({
-    apiKey: process.env.OPENAI_API_KEY,
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-  const res = await api.sendMessage("make my baby go to sleep today");
-  console.log("**LOG** response: ", res);
+
+  const askQuestion = (question) => {
+    readline.question(question || "", async (input) => {
+      const api = new ChatGPTAPI({
+        apiKey: process.env.OPENAI_API_KEY,
+      });
+      const res = await api.sendMessage(input);
+      askQuestion(res.text + " press control+c to exit : ");
+    });
+  };
+  await askQuestion("Ask me a question or say hi! : ");
 };
 
 makeRequest();
